035455520d13baae35bfa4b66779303b96dd6ab2
khannd6 <khanhnd6@gmail.com>
2024-05-02 22:44:52 +0700

    .env

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,10 @@
+require('dotenv').config(); 
+
 var mssqlDBConfig = {
-    user: 'sa',
-    password: '1',
-    server: "LAPTOP-18EFCLPG",
-    database: "ShoesShopping",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     trustServerCertificate: true,
 }
